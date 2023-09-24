238e30916f8e05f550f66cd4136babc36ae25f1d
Oswald <s235440@dtu.dk>
2023-09-24 18:36:29 +0200

    Første rigtige kode

--- diceGame.java
+++ diceGame.java
@@ -1,13 +1,19 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+//Skal laves med en Array 
 class diceRoll {
     public static void main(String[] args) {
-        int player1 = 0;
-        int player2 = 0;
-        Random rand = new Random();
+    
+    Scanner scanner = new Scanner(System.in);
+    //Terning terning = new Terning();
 
-        
+    System.out.println("Navn første spiller");
+    players spiller1 = new players(scanner.nextLine());
+
+    System.out.println("Navn anden spiller");
+    players spiller2 = new players(scanner.nextLine());
 
 
     }
