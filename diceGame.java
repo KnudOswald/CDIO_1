1959f3a896916a03c458e3a0ae8ab6ab06c2f13d
Oswald <s235440@dtu.dk>
2023-09-24 17:57:57 +0200

    Første Start

--- diceGame.java
+++ diceGame.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 class diceRoll {
     public static void main(String[] args) {
+        int player1 = 0;
+        int player2 = 0;
+        Random rand = new Random();
+
+        
+
 
     }
 }
