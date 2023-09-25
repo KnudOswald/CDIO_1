6a4ce77e3a0ca0c3382eaaa4f655b3174cc597f4
zakariye <zakariyeabdulqaadir@gmail.comgit config --global user.email zakariyeabdulqaadir@gmail.com>
2023-09-25 16:05:29 +0200

    version 1.0

--- diceGame.java
+++ diceGame.java
@@ -11,28 +11,51 @@ class diceRoll {
         int turn = 1;
 
         System.out.println("welkome til spillet: ");
+
         System.out.println("tryk på enter");
         s.nextLine();
 
         while (player1 < 40 && player2 < 40) {
-            System.out.println("det er " + turn + "s tur");
-            System.out.println("det er dit tur");
+            System.out.println("det er player" + turn + "s tur");
+
+            System.out.println("det er dit tur tryk på enter");
             s.nextLine();
 
             int dice1 = rand.nextInt(6) + 1;
             System.out.println("du har scoret: " + dice1);
+
             int dice2 = rand.nextInt(6) + 1;
             System.out.println("du har scoret: " + dice2);
+
             int sum = dice1 + dice2;
 
             System.out.println("summet er: " + sum);
 
-            if (dice1 != dice2) {
+            if (!(dice1 == 6 && dice2 == 6)) {
                 if (turn == 1) {
                     turn = 2;
+                    player1 += sum;
+                    System.out.println(" du har: " + player1 + "pionte");
                 } else {
                     turn = 1;
+                    player2 += sum;
+                    System.out.println("du har: " + player2 + "pionte");
                 }
+
+            }
+            if (dice1 == 1 && dice2 == 1) {
+                if (turn == 1) {
+                    player1 = 0;
+                    System.out.println("du har mistede alt dit pointer");
+                } else {
+                    player2 = 0;
+                    System.out.println("du har mistede alt dit pointer.");
+                }
+            }
+            if (player1 >= 40) {
+                System.out.println("player1 er vundet");
+            } else if (player2 >= 40) {
+                System.out.println("player2 er vundet");
             }
 
         }
