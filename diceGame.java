ab32def7beb29b6b2a02fb9faf8210afe308f417
Oswald <s235440@dtu.dk>
2023-09-28 16:50:47 +0200

    V 7.0.2

--- diceGame.java
+++ diceGame.java
@@ -10,29 +10,31 @@ class DiceRoll {
         int player2 = 0;
         int turn = 1;
 
-        System.out.println("welkome til spillet: ");
+        System.out.println("Velkommen til spillet: ");
 
-        System.out.println("tryk på enter");
+        System.out.println("Tryk Enter");
         s.nextLine();
 
-        System.out.println("indtast dit navn player1");
+        System.out.println("Første spiller indtast navn");
         String navn1 = s.nextLine();
 
-        System.out.println("indtast dit navn player 2");
+        System.out.println("Anden spiller indtast navn");
         String navn2 = s.nextLine();
+        // String til at skifte navn ved værd tur
+        String spillerTur = navn1;
 
         while (player1 < 40 && player2 < 40) {
-            System.out.println("det er player" + turn + " s tur");
+            System.out.println("Det er " + spillerTur + "s tur");
 
-            System.out.println("det er dit tur tryk på enter");
+            System.out.println("Tryk på enter");
             s.nextLine();
 
             int dice1 = rand.nextInt(6) + 1;
-            System.out.println("du har scoret: " + dice1);
+            System.out.println("Du har fået: " + dice1);
             // programmet vælger et tilfældige tal som er melle 1 og 6
 
             int dice2 = rand.nextInt(6) + 1;
-            System.out.println("du har scoret: " + dice2);
+            System.out.println("Du har fået: " + dice2);
             // programmet vælger et tilfældige tal som er melle 1 og 6
 
             int sum = dice1 + dice2;
@@ -43,30 +45,35 @@ class DiceRoll {
                 if (turn == 1) {
                     if (dice1 == 1 && dice2 == 1) {
                         sum = 0; // nulstiller pointerne.
-                        System.out.println("du har mistede alt dit pointer");
+                        System.out.println("Du har mistede alle dine point");
                         turn = 2;
                     } else {
                         turn = 2;
                     }
                     player1 += sum; // summer pointerne.
-                    System.out.println(" du har: " + player1 + "pionte");
+                    System.out.println("Du har nu: " + player1 + " piont" + "\n");
                 } else {
                     if (dice1 == 1 && dice2 == 1) {
                         sum = 0; // nulstiller pionterne.
-                        System.out.println("du har mistede alt dit pointer");
+                        System.out.println("Du har mistede alle dine point");
                         turn = 1;
                     } else {
                         turn = 1;
                     }
                     player2 += sum;
-                    System.out.println("du har: " + player2 + " pionte");
-                }
-
+                    System.out.println("Du har nu: " + player2 + " pionts" + "\n");
+                }    
             }
+            //det der gør at den skifter navn ved tur skifte hver gang
+            if (spillerTur.equals(navn1)){
+            spillerTur = navn2;
+            } else {
+            spillerTur = navn1;
+        }
             if (player1 >= 40) {
-                System.out.println("player1 er vundet " + navn1);
+                System.out.println("Vinderne er: " + navn1);
             } else if (player2 >= 40) {
-                System.out.println("player2 er vundet " + navn2);
+                System.out.println("Vinderen er: " + navn2);
             } // udråber vineren.
 
         }
